d1d49735559cb20f24fb628f0d7154f92361f530
Emily Jirles <emily.jirles@gmail.com>
2018-05-02 16:32:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
-end
+
+  attr_accessor :a, :b, :c 
+
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end
+  
+  def valid?
+    self.a + self.b > self.c && self.c + self.b > self.a && self.a + self.c > self.b && self.a > 0 && self.b > 0 && self.c > 0
+  end
+  
+  def kind
+    if !self.valid?
+      begin 
+        raise TriangleError
+      rescue TriangleError => error 
+        puts error.message
+      end
+    else 
+      if self.a =+ self.b || self.b == self.c || self.c == self.a
+        kind = 'isosceles'
+      elsif self.a == self.b && self.a == self.c && self.b == self.c
+        kind = 'equilateral'
+      elsif self.a != self.b && self.a != self.c && self.b != self.c
+        kind = 'scalene'
+      end
+    end
+    kind 
+  end
+
+  class TriangleError < StandardError
+    
+    def message
+      "the triangle is invalid"
+    end
+    
+  end
+
+end  
